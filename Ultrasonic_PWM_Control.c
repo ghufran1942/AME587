f1311c127877538aff4824d88e725c4e4069f286
Mohammad Ghufran <ghufran1942@arizona.edu>
2023-03-31 14:57:53 -0700

    Updated US code

--- Ultrasonic_PWM_Control.c
+++ Ultrasonic_PWM_Control.c
@@ -12,10 +12,25 @@
 
 #pragma config FOSC = INTRCIO, WDTE = OFF, PWRTE = OFF, MCLRE = ON, CP = OFF, CPD = OFF, BOREN = OFF, IESO = OFF, FCMEN = OFF
 
+// Global variable
+volatile uint8_t timer0_overflow_count = 0;
+
+// Function Declarations
+int initialization();
+uint16_t measure_duration();
+float calcualte_distance(uint16_t duration);
+void Send(unsigned char x);
+unsigned char Receive(void);
+void __interrupt() ISR(void);
+
 int main(){
-    Initialization();
+    initialization();
+
+    __delay_ms(100);
+
     PORTC = 0;
     CCPR1L = 0;
+    
     while (1) {
         __delay_us(20);
         GO = 1;                     // Begin AD Conversion
@@ -25,6 +40,45 @@ int main(){
         //Toggle LEDs/motor 1
         CCPR1L = Receive();
     }
+}
+
+uint16_t measure_duration()
+{
+    // Signal Initialization
+    RA0 = 1;
+    __delay_us(10);
+    RA0 = 0;
+
+    TMR1 = 0;                   // Reset Timer1
+    T1CONbits.TMR1ON = 1;       // Enable Timer1
+
+    // Start counting
+    timer0_overflow_count = 0;  // Reset the overflow count
+    TMR0 = 0;                   // Reset Timer0 value
+
+    while (PORTBbits.RB4 == 0 && timer0_overflow_count < 20) {} // Wait for the echo pulse to start
+
+    if (timer0_overflow_count >= 20) {
+        T1CONbits.TMR1ON = 0;   // Disable Timer1
+        return 0;               // If timed out, return 0
+    }
+
+    TMR1 = 0;                   // Reset the timer
+
+    while (PORTBbits.RB4 == 1 && timer0_overflow_count < 20) {} // Wait for the echo pulse to end
+
+    T1CONbits.TMR1ON = 0;       // Disable Timer1
+
+    if (timer0_overflow_count >= 20) return 0; // If timed out, return 0
+
+    unsigned short duration = TMR1; // Read the timer value
+
+    // return duration * 0.034 / 2;
+    return duration;
+}
+
+float calcualte_distance(uint16_t duration){
+    return duration * 0.0135 / 2; // I think this should calcualte the distance in inches
 }
 
 void Send(unsigned char x)            // Send 1 Byte to MATLAB via RS232
@@ -41,19 +95,31 @@ unsigned char Receive(void)                   // Receive 1 Byte from MATLAB via 
     return RCREG;
 } 
 
+void __interrupt() ISR(void)
+{
+    if (INTCONbits.T0IF)        // Check if Timer0 overflow interrupt occurred
+    {
+        timer0_overflow_count++;// Increment the overflow count
+        TMR0 = 0;               // Reset Timer0 value
+        INTCONbits.T0IF = 0;    // Clear the Timer0 overflow interrupt flag
+    }
+}
+
 int initialization(){
     // BANK3
     PSTRCON = 0b00010011;           //PA & PB = PWM; PC & PD = port pins; steering update at beginning
     
     // BANK2
-    ANSEL  = 0b00000001;            // All pins digital except pin AN0
+    ANSEL  = 0b00000000;            // All pins digital except pin AN0
     ANSELH = 0b00000000;            // All pins digital
     
     // BANK1
     OSCCON = 0b01110000;            // Setting Oscillator to do 8MHz
     ADCON1 = 0b01010000;            // Select ADC Clock to FOSC/16
-    TRISA  = 0b00000001;            // Input: pin AN0
+    TRISA = 0b00000101;            // Input: pin AN0 pin A2 as digitial input
+    TRISB = 0b00010000;
     TRISC  = 0b00000000;            // Output: all pins
+
     //TXSTA bits
     TRMT = 1;                       // Empty Transmit Shift register
     BRGH = 1;                       // set Baud Rate high
@@ -66,6 +132,7 @@ int initialization(){
     //ADCON0
     ADON = 1;                       // Enable ADC conversion
     GO = 0;                         // Stop any conversion
+
     //ADC to AN0
     CHS0 = 0;
     CHS1 = 0;
@@ -77,5 +144,17 @@ int initialization(){
     PR2 = 255;
     CCPR1L = 0b00000000;
     T2CON = 0b01111110;
+
+    // Timer1 configuration (for measuring echo pulse duration)
+    T1CONbits.TMR1CS = 0;       // Select internal clock (FOSC/4)
+    T1CONbits.TMR1ON = 0;       // Disable Timer1 (it will be enabled in the US_distance function)
+
+    // Timer0 configuration (for handling timeout)
+    OPTION_REGbits.PSA = 0;     // Assign prescaler to Timer0
+    OPTION_REGbits.PS = 0b010;  // Set the prescaler to 1:8
+    OPTION_REGbits.T0CS = 0;    // Select internal instruction cycle clock
+    OPTION_REGbits.T0SE = 0;    // Increment on low-to-high transition on T0CKI pin
+    TMR0 = 0;
+
     return 0;
 }
--- ultrasonic-V2.c
+++ ultrasonic-V2.c
@@ -5,13 +5,17 @@
 
 #include <xc.h>
 #include <pic16f690.h>
+#include <stdint.h>
 
 #define _XTAL_FREQ 8000000
 
 #pragma config FOSC = INTRCIO, WDTE = OFF, PWRTE = OFF, MCLRE = ON, CP = OFF, CPD = OFF, BOREN = OFF, IESO = OFF, FCMEN = OFF
 
+volatile uint8_t timer0_overflow_count = 0;
+
 //Function Declarations
-float measure_distance();
+unsigned short measure_duration();
+float calculate_distance(uint16_t duration);
 void Send(unsigned char x);
 unsigned char Receive(void);
 
@@ -24,7 +28,18 @@ int main()  // Start of the main function
     OSCCON = 0b01110000;            // Setting Oscillator to 8MHz
     TRISAbits.TRISA0 = 0;           // PORTA0 as output => Trigger
     TRISBbits.TRISB4 = 1;           // PORTB4 as input => Echo
-
+    
+    T1CONbits.TMR1CS = 0;       // Select internal clock (FOSC/4)
+    T1CONbits.TMR1ON = 0;       // Disable Timer1 (it will be enabled in the US_distance function)
+    
+    OPTION_REGbits.PSA = 0;     // Assign prescaler to Timer0
+    OPTION_REGbits.PS0 = 0;
+    OPTION_REGbits.PS1 = 1;
+    OPTION_REGbits.PS2 = 0;
+    OPTION_REGbits.T0CS = 0;    // Select internal instruction cycle clock
+    OPTION_REGbits.T0SE = 0;    // Increment on low-to-high transition on T0CKI pin
+    TMR0 = 0;
+    
     // Set up UART
     TRISCbits.TRISC6 = 1;           // Set TX pin as input
     TRISCbits.TRISC7 = 1;           // Set RX pin as input
@@ -37,37 +52,51 @@ int main()  // Start of the main function
     RCSTAbits.CREN = 1;             // Enable receiver
 
     __delay_ms(100);                // Wait for serial communication to stabilize
-
+    
+    uint16_t prev_duration = -1;
     while(1){
         __delay_us(20);
 
-        float distance = measure_distance();
-
+        uint16_t curr_duration = measure_duration();
+        
+        if (prev_duration != -1){ // Only compare if there is a valid previous value
+            uint8_t range = 0.1 * prev_duration; // depending on the constant 0.1 the range changes.
+            uint16_t lower_bound = prev_duration - range;
+            uint16_t upper_bound = prev_duration + range;
+            if (curr_duration >= lower_bound && curr_duration <= upper_bound) {
+                curr_duration = 10;
+            }else{
+                prev_duration = curr_duration; // Store the current duration value as the previous duration value for the next iteration
+            }
+        }       
+        float distance = calculate_distance(curr_duration);
+        
         char *bytes = (char*)(&distance);
-        for(int i=0; i < 4; i++){
+        for(int i=0; i < 4;i++)
+        {
             Send(bytes[i]);
         }
-        
+
         PORTC = Receive();
     }
     return 0;
 }
 
-float measure_distance()
+unsigned short measure_duration()
 {
     // Signal Initialization
     RA0 = 1;
     __delay_us(10);
     RA0 = 0;
 
-    TMR1 = 0;
+    TMR0 = 0;
     // Start counting
 
     while (PORTBbits.RB4 == 0);     // Wait for the echo pulse to start
     while (PORTBbits.RB4 == 1);     // Wait for the echo pulse to end
-    unsigned short duration = TMR1; // Read the timer value
+    unsigned short duration = TMR0; // Read the timer value
 
-    return duration * 0.034 / 2;
+    return duration;
 }
 
 void Send(unsigned char x)          // Send 1 Byte to MATLAB via RS232
@@ -81,3 +110,10 @@ unsigned char Receive(void)         // Receive 1 Byte from MATLAB via RS232
     while (!RCIF);                  // Wait for RCREG to fill (PIR1 reg)
     return RCREG;                   // Move Receive Data Register to func output  
 }
+
+float calculate_distance(uint16_t curr_duration){
+    float distance = curr_duration * 0.0135 / 2; // I think this should calculate the distance in inches
+    return distance;
+}
+
+
