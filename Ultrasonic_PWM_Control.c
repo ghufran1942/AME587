7bcfa4931a53afd53312672d6198f23a278211b4
Mohammad Ghufran <ghufran1942@arizona.edu>
2023-03-31 14:03:14 -0700

    Initial US_PWM_Cont

---
+++ Ultrasonic_PWM_Control.c
@@ -0,0 +1,25 @@
+//    Filename: Project
+//    Instructor:
+//    Group: 3
+//    Date: 3/23/2023
+
+#include <xc.h>
+#include <pic16f690.h>
+#include <stdint.h>
+
+#define _XTAL_FREQ 8000000
+
+#pragma config FOSC = INTRCIO, WDTE = OFF, PWRTE = OFF, MCLRE = ON, CP = OFF, CPD = OFF, BOREN = OFF, IESO = OFF, FCMEN = OFF
+
+int main(){
+
+}
+
+int initialization(){
+    
+    
+
+
+
+    return 0;
+}
