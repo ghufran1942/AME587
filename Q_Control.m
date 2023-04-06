ebd3114752d4738090c55e1070d3a469dbe32702
Julio-E-Trejo <rtjulio119@arizona.edu>
2023-04-06 14:53:19 -0700

    04_06_2023 Update

--- Q_Control.m
+++ Q_Control.m
@@ -34,7 +34,7 @@ for k=1:10 %Number of training targets for the Q-table
 
     x(1)=x_ini;
 
-    for i=1:N %while 
+    for i=1:N %while true %loop will not break until target is reached
         % chose an action from state x(i)
 
         [~,a] =  max(Q(x(i),:));
@@ -60,9 +60,9 @@ for k=1:10 %Number of training targets for the Q-table
         Q(x(i),a)=Q(x(i),a)+alp*(r(i)-gam*Q(x(i+1),a_next)-Q(x(i),a));
 
         xold(k,i)=x(i);
-        for i = 1:length(Time)
+        for j = 1:length(Time)
            cur_dur = fread(S,1,'uint16'); % Read 2 bytes (16 bits) from the Microcontroller
-           range = 0.1*prev_dur;
+           range = 37.5; %1 inch ~ 150 us
            LB = prev_dur - range;
            UB = prev_dur + range;
            dist = prev_dur * 0.0135 / 2;
@@ -70,17 +70,26 @@ for k=1:10 %Number of training targets for the Q-table
                prev_dur = cur_dur;
                % PWM for motor 1/driver outputs 1&2
                % PWM comes from q-table
-               % [PWM1,PWM2] = QTABLE_Action;
+               % [_,PWM1] = max(Q(x(i),:));
+               % PWM2 = 255 - PWM1;
+               fwrite(S,1,'uint8'); % Send 1 byte back to the Microcontroller
+               fwrite(S,100,'uint8'); % Send 1 byte back to the Microcontroller
+               pause(2);
+               fwrite(S,200,'uint8'); % Send 1 byte back to the Microcontroller
+           else
+               fwrite(S,0,'uint8'); % Send 1 byte back to the Microcontroller
+               fwrite(S,100,'uint8'); % Send 1 byte back to the Microcontroller
+               fwrite(S,100,'uint8'); % Send 1 byte back to the Microcontroller
+               continue;
            end
-           fwrite(S,PWM1,'uint8'); % Send 1 byte back to the Microcontroller
-           fwrite(S,PWM2,'uint8'); % Send 1 byte back to the Microcontroller
-           Time(i) = toc;
-           addpoints(Plot,Time(i),dist);
+           
+           Time(j) = toc;
+           addpoints(Plot,Time(j),dist);
            axis([toc-10 toc+1 -10 300]); % Axis based on elapsed time
            legend('Analog input')
         
            %Q-update
-           Q(x(i),a)=Q(x(i),a)+alp*(r(i)-gam*Q(x(i+1),a_next)-Q(x(i),a));
+           Q(x(j),a)=Q(x(j),a)+alp*(r(j)-gam*Q(x(j+1),a_next)-Q(x(j),a));
            pause(0.01);
         end
     end
@@ -89,55 +98,52 @@ for k=1:10 %Number of training targets for the Q-table
 
 end
 
-x(1)=randi(20); %randi([min(xold) max(xold)]);
+% x(1)=randi(20); %randi([min(xold) max(xold)]);
 
  
 
-for i=1:N,
-
-    % chose an action from state x(i)
-
-    [~,a] = max(Q(x(i),:));
-    %Write to pic ==> wait for response as x(i+1)
-    fwrite(S,4,'uint8');
-    pause(0.01);
-    x(i+1) = fread(S,1,'uint8'); %New position
-
-    if x(i+1)>=20, x(i+1)=20;end
-
-    if x(i+1)<=1, x(i+1)=1;end
-
-    [~,a_next] = max(Q(x(i+1),:));  % get future action
-
-    if abs(x(i+1)-xT)< abs(x(i)-xT), r(i)=0;
-
-    else r(i)=-1; end
-
-end
+% for i=1:N,
+% 
+%     % chose an action from state x(i)
+% 
+%     [~,a] = max(Q(x(i),:));
+%     %Write to pic ==> wait for response as x(i+1)
+%     fwrite(S,4,'uint8');
+%     pause(0.01);
+%     x(i+1) = fread(S,1,'uint8'); %New position
+% 
+%     if x(i+1)>=20, x(i+1)=20;end
+% 
+%     if x(i+1)<=1, x(i+1)=1;end
+% 
+%     [~,a_next] = max(Q(x(i+1),:));  % get future action
+% 
+%     if abs(x(i+1)-xT)< abs(x(i)-xT), r(i)=0;
+% 
+%     else r(i)=-1; end
+% 
+% end
 
 
 %%Plotting/Communication section
-
-
-
 fclose(S);
 delete(S);
 clear S; % Close and clear serial port
-figure
-
-hold
-
-for k=1:10,plot(xold(k,:));end
-
-plot(x,'r')
-xlim([0 21])
-
-legend({'Training data','Training data','Training data','Training data','Training data','Training data','Training data','Training data','Training data','Training data', 'Trained'})
-
-xlabel('Time, t');ylabel('State,s(t)')
-
-shg
-
-figure
-
-bar3(Q+1);shg
+% figure
+% 
+% hold
+% 
+% for k=1:10,plot(xold(k,:));end
+% 
+% plot(x,'r')
+% xlim([0 21])
+% 
+% legend({'Training data','Training data','Training data','Training data','Training data','Training data','Training data','Training data','Training data','Training data', 'Trained'})
+% 
+% xlabel('Time, t');ylabel('State,s(t)')
+% 
+% shg
+% 
+% figure
+% 
+% bar3(Q+1);shg
